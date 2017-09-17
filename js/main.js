92a552bd3826298dfacfe96f4c4b43c42f8c8f6e
Nate Welfare <nwelfare@gmail.com>
2017-09-17 13:35:14 +0100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,20 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+	}
+	else if (cardsInPlay[0] !== cardsInPlay[1]){
+		alert("Sorry try again");
+	}
+
+	
+}
+
+console.log("User flipped " + cardsInPlay[0]);
+console.log("User flipped " + cardsInPlay[1]);
