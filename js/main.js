9742be5ef7784756060106bebd8e1394fdf493fc
Nate Welfare <nwelfare@gmail.com>
2017-09-17 13:45:44 +0100

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 console.log("Up and running!");
-var cards = ["queen","queen","king","king"];
+var cards = [
+{
+rank: 'queen',
+suit: 'hearts',
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank: 'queen',
+suit: 'diamonds',
+cardImage: "images/queen-of-diamonds.png"
+},
+{
+rank: 'king',
+suit: 'hearts',
+cardImage: "images/king-of-hearts.png"
+},
+{
+rank: 'king',
+suit: 'diamonds',
+cardImage: "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
 var checkForMatch = function(){
 	if (cardsInPlay[0] === cardsInPlay[1]){
@@ -9,12 +30,15 @@ var checkForMatch = function(){
 	}
 }
 var flipCard = function(cardId){
-	cardsInPlay.push(cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
 if (cardsInPlay.length === 2) {
-	checkForMatch();
+checkForMatch();
 }
-console.log("User flipped " + cards[cardId])
+console.log("User flipped " + cards[cardId].rank)
+console.log(cards[cardId].cardImage);
+console.log(cards[cardId].suit);
+}
 
-}
 flipCard(0);
 flipCard(2);
+
